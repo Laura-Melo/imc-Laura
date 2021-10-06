6b202bcaa8cafafed8474e2f17ba50d88f5883c8
Laura Gabriela <78507609+Laura-Melo@users.noreply.github.com>
2021-10-06 16:34:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 function calcular() {
 
-    //pega valores inseridos, e substitui virgula por ponto
     var nome = document.getElementById('nome').value.replace(/,/g, '.');;
     var peso = document.getElementById('peso').value.replace(/,/g, '.');;
     var altura = document.getElementById('altura').value.replace(/,/g, '.');;
-  
-    // valida se os campos não estão vazios
+
     if (peso == '' || altura == '') {
       document.getElementById("message").innerHTML = 'preencha os campos';
       return false;
     }
-  
-    // calcula IMC
+ 
     var resultado = (peso / (altura ** 2));
     var message = '';
-  
-    // verifica situação
+
     if (resultado < 17) {
       message = 'Você está muito abaixo do peso';
     } else if (resultado >= 17 && resultado <= 18.49) {
@@ -32,10 +28,11 @@ function calcular() {
       message = 'Você tem Obesidade nível III';
     }
   
-    // mostra resultado
-
     resultado = resultado.toFixed(2).replace(/\./g, ',');
 
 
     document.getElementById("message").innerHTML = 'Olá ' + nome + '!' + '&nbsp' +'Como' + '&nbsp' + peso + 'kg' + '&nbsp' + 'e' + '&nbsp' + altura + 'm' + '<br>'+' O seu IMC é' + '&nbsp' + resultado + '<br>' + message;
   }
+
+
+  
